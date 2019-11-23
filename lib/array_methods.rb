a555e7d0288d2455809e1f21d4d817224755eb5c
Noah Scala <tobi8912@gmail.com>
2019-11-23 22:19:57 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,10 @@ def find_element_index(array, value_to_find)
   # Add your solution here
   count = 0
   while count < array.length do
-    if array[count] ==
+    if array[count] == value_to_find
+      count
+    end
+    count += 1
   end
 end
 
